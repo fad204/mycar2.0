51966a9fa46cbfbac700fbff2be43eff9cd098bd
fad204 <fadona@g.syr.edu>
2026-05-23 22:42:52 +0200

    Update MyCarMod.java

--- src/main/java/net/mycar/MyCarMod.java
+++ src/main/java/net/mycar/MyCarMod.java
@@ -3,7 +3,10 @@ package net.mycar;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.mycar.block.TollCameraBlock;
+import net.mycar.block.TollCameraBlockEntity;
 import net.mycar.entity.AbstractVehicleEntity;
 import net.mycar.entity.BicycleEntity;
 import net.mycar.entity.CarEntity;
@@ -11,11 +14,16 @@ import net.mycar.entity.TruckEntity;
 import net.mycar.item.BicycleItem;
 import net.mycar.item.CarItem;
 import net.mycar.item.FuelCanItem;
+import net.mycar.item.RfcCoinItem;
 import net.mycar.item.TruckItem;
 import net.mycar.network.Networking;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -42,6 +50,31 @@ public class MyCarMod implements ModInitializer {
     public static final Item FUEL_CAN = new FuelCanItem(
         new FabricItemSettings().group(ItemGroup.MISC).maxCount(16)
     );
+
+    // ---------------- RFC currency coins ----------------
+    // Field-static so they can be referenced from RfcCurrency without an extra map.
+    public static final RfcCoinItem RFC_5    = new RfcCoinItem(coinSettings(), 5);
+    public static final RfcCoinItem RFC_10   = new RfcCoinItem(coinSettings(), 10);
+    public static final RfcCoinItem RFC_50   = new RfcCoinItem(coinSettings(), 50);
+    public static final RfcCoinItem RFC_100  = new RfcCoinItem(coinSettings(), 100);
+    public static final RfcCoinItem RFC_500  = new RfcCoinItem(coinSettings(), 500);
+    public static final RfcCoinItem RFC_1000 = new RfcCoinItem(coinSettings(), 1000);
+
+    private static FabricItemSettings coinSettings() {
+        return new FabricItemSettings().group(ItemGroup.MISC).maxCount(64);
+    }
+
+    // ---------------- Toll camera block ----------------
+    public static final Block TOLL_CAMERA = new TollCameraBlock(
+        FabricBlockSettings.of(Material.METAL).strength(3.0f, 6.0f).requiresTool().nonOpaque()
+    );
+
+    public static final BlockItem TOLL_CAMERA_ITEM = new BlockItem(
+        TOLL_CAMERA, new FabricItemSettings().group(ItemGroup.REDSTONE)
+    );
+
+    public static final BlockEntityType<TollCameraBlockEntity> TOLL_CAMERA_BE =
+        BlockEntityType.Builder.create(TollCameraBlockEntity::new, TOLL_CAMERA).build(null);
 
     // ---------------- Entity types ----------------
     public static final EntityType<CarEntity> CAR = Registry.register(
@@ -92,6 +125,19 @@ public class MyCarMod implements ModInitializer {
             BICYCLE_ITEMS.put(v, bike);
         }
         Registry.register(Registry.ITEM, id("fuel_can"), FUEL_CAN);
+
+        // RFC coins
+        Registry.register(Registry.ITEM, id("rfc_5"),    RFC_5);
+        Registry.register(Registry.ITEM, id("rfc_10"),   RFC_10);
+        Registry.register(Registry.ITEM, id("rfc_50"),   RFC_50);
+        Registry.register(Registry.ITEM, id("rfc_100"),  RFC_100);
+        Registry.register(Registry.ITEM, id("rfc_500"),  RFC_500);
+        Registry.register(Registry.ITEM, id("rfc_1000"), RFC_1000);
+
+        // Toll camera block + its item form + its block-entity type
+        Registry.register(Registry.BLOCK, id("toll_camera"), TOLL_CAMERA);
+        Registry.register(Registry.ITEM,  id("toll_camera"), TOLL_CAMERA_ITEM);
+        Registry.register(Registry.BLOCK_ENTITY_TYPE, id("toll_camera"), TOLL_CAMERA_BE);
 
         // Network handlers (shared between car and truck)
         ServerPlayNetworking.registerGlobalReceiver(Networking.SHIFT_GEAR, Networking::handleShiftGear);
